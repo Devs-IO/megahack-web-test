dc71e7c39fdc5c647e4a494a4c5163ee48738a61
lucaspontoexe <ltx16@hotmail.com>
2020-02-18 11:50:04 -0300

    add card themes

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,20 +1,26 @@
 import React from "react";
-import "./styles.css";
 import visa from "../../assets/cards/visa.png";
 import master from "../../assets/cards/mastercard.svg";
 import eye from "../../assets/icons/olho.svg";
+import "./styles.css";
 
 const cardtypes = {
   visa,
   master
 };
+
 export default function Card({ type, end, limit }) {
+  const themes = {
+    visa: { backgroundColor: "white", color: "#1a3d3d" },
+    master: { backgroundColor: "#347474", color: "white" }
+  };
+
   return (
     <div className="card-container">
-      <div className="card">
+      <div className="card" style={themes[type]}>
         <img className="cardtype" src={cardtypes[type]} alt="visa" />
         <img className="eye" src={eye} alt="mas olha la" />
-        <div className="cardtext">**** **** **** {end}</div>
+        <div className="cardtext" style={themes[type]}>**** **** **** {end}</div>
       </div>
 
       <div className="card-limit">
@@ -22,7 +28,8 @@ export default function Card({ type, end, limit }) {
           <span className="limit-text">R${limit} </span>
           <span>de limite</span>
         </div>
-        
+
+        {/* <ProgressBar /> */}
       </div>
     </div>
   );
